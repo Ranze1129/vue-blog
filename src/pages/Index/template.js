f470397e8cb5c273af063a940f6f3756b91d8d62
Ranze <1066467243@qq.com>
2022-04-09 15:12:39 +0800

    基本完成首页

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -1,8 +1,30 @@
-import request from '../../helpers/request.js'
-import auth from '../../api/auth.js'
-import blog from '../../api/blog.js'
+import blog from '@/api/blog.js'
 
 
 export default {
-
+  data() {
+    return {
+      blogs: [],
+      total: 0,
+      page: 1
+    }
+  },
+  created() {
+    this.page = parseInt(this.$route.query.page) ||1
+    blog.getIndexBlogs({page:this.page}).then(res => {
+      this.blogs = res.data
+      this.total = res.total
+      this.page = res.page
+    })
+  },
+  methods: {
+    onPageChange(newPage) {
+      blog.getIndexBlogs({page: newPage}).then(res => {
+        this.blogs = res.data
+        this.total = res.total
+        this.page = res.page
+     this.$router.push({path:'/',query:{page:newPage}})
+      })
+    }
+  }
 }
